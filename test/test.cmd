835245b5e9b3afb4e7614a61ede5463f51ef5a4a
= <=>
2021-05-27 16:15:20 +0200

    update: test.cmd

--- test/test.cmd
+++ test/test.cmd
@@ -4,16 +4,16 @@ cargo build
 echo ================================================
 echo    TEST 1 : this is supposed to fail on schema
 echo ================================================
-"./target/debug/cityjson-validator.exe" D:\Dev\Geo\data\cityjson/schema.json D:\Dev\Geo\data\cityjson/den-haag-screwed.json
+"./target/debug/cityjson-validator.exe" D:\Dev\Geo\data\cityjson\schema.json D:\Dev\Geo\data\cityjson\den-haag-screwed.json
 
 echo ================================================
 echo  TEST 2 : this is supposed to fail on dup verts
 echo ================================================
-"./target/debug/cityjson-validator.exe" D:\Dev\Geo\data\cityjson/schema.json D:\Dev\Geo\data\cityjson/den-haag-dup-verts.json
+"./target/debug/cityjson-validator.exe" D:\Dev\Geo\data\cityjson\schema.json D:\Dev\Geo\data\cityjson\den-haag-dup-verts.json
 
 echo ================================================
 echo       TEST 3 : this is supposed to work
 echo ================================================
-"./target/debug/cityjson-validator.exe" D:\Dev\Geo\data\cityjson/schema.json D:\Dev\Geo\data\cityjson/den-haag.json
+"./target/debug/cityjson-validator.exe" D:\Dev\Geo\data\cityjson\schema.json D:\Dev\Geo\data\cityjson\den-haag.json
 
 
